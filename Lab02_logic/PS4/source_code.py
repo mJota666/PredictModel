d2f731ef75a238c85af3ce425085615d4a2bf973
mJota666 <micode2744@gmail.com>
2024-08-11 19:45:53 +0700

    change PL logic

--- Lab02_logic/PS4/source_code.py
+++ Lab02_logic/PS4/source_code.py
@@ -2,22 +2,16 @@ import os
 import time
 import itertools
 
-INPUT_PATH = 'c:/Users/nguye/OneDrive/Desktop/Lab02_logic/PredictModel/Lab02_logic/Input'
+INPUT_PATH = 'C:/Users/nguye/OneDrive/Desktop/Lab02_logic/PredictModel/Lab02_logic/Input'
 OUTPUT_PATH = 'C:/Users/nguye/OneDrive/Desktop/Lab02_logic/PredictModel/Lab02_logic/Output'
 
 class KnowldegeBase:
     def __init__(self, statement, size, clauses):
         self.statement = statement
         self.size = size
-        self.negation_statement = self.negation_atom()
+        self.negative_statement = self.get_negative_statement()
         self.clauses = clauses
-    def print_clauses(self):
-        for clause in self.clauses:
-            print(clause)
-    def append_clause(self, clause):
-        if clause not in self.clauses:
-            self.clauses.append(clause)
-    def negation_atom(self):
+    def get_negative_statement(self):
         negations = []
         for atom in self.statement:
             if atom[0] == '-':
@@ -25,88 +19,79 @@ class KnowldegeBase:
             else:
                 negations.append('-' + atom)
         return negations
-
-def get_negative_atom(atom):
-    if atom[0] == '-':
-        return atom[1:]
-    else:
-        return '-' + atom
-    
-def check_complementary(clause):
-    for atom in clause:
-        if get_negative_atom(atom) in clause:
-            return True
-    return False
-
-def normClause(clause):
-    clause = list(dict.fromkeys(clause))
-    print(clause)
-    tuple_form = []
-    for atom in clause:
+    def get_negative_atom(self, atom):
         if atom[0] == '-':
-            tuple_form.append((atom[1], -1))
+            return atom[1:]
         else:
-            tuple_form.append((atom[0], 1))
-    tuple_form.sort()
-    res = []
-    for tup in tuple_form:
-        if tup[1] == -1:
-            res.append('-' + tup[0])
-        else:
-            res.append(tup[0])
-    return res
-
-def resolve(new_kb, clause_1, clause_2):
-    new_clause = []
-    for atom in clause_1:
-        neg_atom = get_negative_atom(atom)
-        if neg_atom in clause_2:
-            temp_c_i = clause_1.copy()
-            temp_c_j = clause_2.copy()
-            temp_c_i.remove(atom)
-            temp_c_j.remove(neg_atom)
-            if not temp_c_i and not temp_c_j:
-                new_clause.append('{}')
-            else:
-                clause = temp_c_i + temp_c_j
-                clause = normClause(clause)
-                if not check_complementary(clause) and clause not in new_kb:
-                    new_clause = clause
-    return new_clause
-
-def check_satisfaction(kb, old_clause, new_clause):
-    if old_clause == new_clause:
+            return '-' + atom
+    def check_complementary(self, clause):
+        for atom in clause:
+            if self.get_negative_atom(atom) in clause:
+                return True
         return False
-    elif all(item in new_clause for item in old_clause):
-        return False 
-    elif new_clause == []:
-        return False      
-    elif new_clause in kb:
-        return False 
-    else:
-        return True
+    def norm_clause(self, clause):
+        clause = list(dict.fromkeys(clause))
+        tuple_form = []
+        for atom in clause:
+            if atom[0] == '-':
+                tuple_form.append((atom[1], -1))
+            else:
+                tuple_form.append((atom[0], 1))
+        tuple_form.sort()
+        res = []
+        for tup in tuple_form:
+            if tup[1] == -1:
+                res.append('-' + tup[0])
+            else:
+                res.append(tup[0])
+        return res
+    def check_satisfaction(self, kb, old_clause, new_clause):
+        if old_clause == new_clause:
+            return False
+        elif not new_clause:
+            return False      
+        else:
+            return True
+    def resolve(self, new_kb, clause_1, clause_2):
+        new_clause = []
+        for atom in clause_1:
+            neg_atom = self.get_negative_atom(atom)
+            if neg_atom in clause_2:
+                new_clause_1 = clause_1[:]
+                new_clause_2 = clause_2[:]
+                new_clause_1.remove(atom)
+                new_clause_2.remove(neg_atom)
+                if not new_clause_1 and not new_clause_2:
+                    new_clause.append('{}')
+                else:
+                    clause = new_clause_1 + new_clause_2
+                    clause = self.norm_clause(clause)
+                    if not self.check_complementary(clause) and clause not in new_kb:
+                        new_clause = clause
+        return new_clause
+    def PL_Resolution(self, knowledge_base):
+        new_kb = knowledge_base.clauses[:]
+        for statement in knowledge_base.negative_statement:
+            new_kb.append([statement])
+        statement = knowledge_base.statement
+        new_clauses = []
+        while 1:
+            bases = []            
+            for clause1 in new_kb:
+                for clause2 in new_kb:
+                    if clause1 != clause2:
+                        resolvent = self.resolve(new_kb, clause1, clause2)
+                        if self.check_satisfaction(new_kb, clause1, resolvent):
+                            if resolvent not in bases and resolvent not in new_kb:
+                                bases += [resolvent]
+                                if ['{}'] in bases:
+                                    new_clauses += [bases]
+                                    return new_clauses, True
+            new_kb += bases
+            new_clauses += [bases]
+            if not bases:
+                return new_clauses, False
 
-def PL_Resolution(knowledge_base):
-    new_kb = knowledge_base.clauses[:]
-    for statement in knowledge_base.negation_statement:
-        new_kb.append([statement])
-    statement = knowledge_base.statement
-    new_clauses = []
-    while 1:
-        bases = []            
-        for clause1 in new_kb:
-            for clause2 in new_kb:
-                if clause1 != clause2:
-                    resolvent = resolve(new_kb, clause1, clause2)
-                    if check_satisfaction(new_kb, clause1, resolvent):
-                        if resolvent not in bases and resolvent not in new_kb:
-                            bases += [resolvent]
-        new_kb += bases
-        new_clauses += [bases]
-        if not bases:
-            return new_clauses, False
-        elif ['{}'] in bases:
-            return new_clauses, True
 
 def extract_or(clause):
     clause = clause.split()
@@ -114,7 +99,7 @@ def extract_or(clause):
     clause.sort(key=lambda x: x.lower().lstrip('-'))
     return clause
 
-def handle_file(file_path):
+def read_file(file_path):
     try:
         with open(file_path) as file:
             content = file.read().splitlines()
@@ -128,15 +113,6 @@ def handle_file(file_path):
             return statement, size, new_clauses
     except:
         print("Cannot open", file_path)
-
-def read_file():
-    file_names = os.listdir(INPUT_PATH)
-    for index, file_name in enumerate(file_names):
-        if file_name.endswith('.txt'):
-            statement, size, clauses = handle_file(os.path.join(INPUT_PATH, file_name))
-            knowledge_base = KnowldegeBase(statement, size, clauses)
-            new_set, check = PL_Resolution(knowledge_base)
-            write_file(OUTPUT_PATH + "/output" + str(index + 1).zfill(2) + '.txt', new_set, check)
 
 def write_file(file_name, new_set, check):
     with open(file_name, 'w') as file:
@@ -154,7 +130,13 @@ def write_file(file_name, new_set, check):
             file.write("NO")
 
 def main():
-    read_file()
+    file_names = os.listdir(INPUT_PATH)
+    for index, file_name in enumerate(file_names):
+        if file_name.endswith('.txt'):
+            statement, size, clauses = read_file(os.path.join(INPUT_PATH, file_name))
+            knowledge_base = KnowldegeBase(statement, size, clauses)
+            new_set, check = knowledge_base.PL_Resolution(knowledge_base)
+            write_file(OUTPUT_PATH + "/output" + str(index + 1).zfill(2) + '.txt', new_set, check)
 
 if __name__ == '__main__':
     main() 
